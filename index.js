fee6653f3c4e5c7c00ef12b111faa3e06011de02
William <67127399+thunder-red-star@users.noreply.github.com>
2022-01-03 18:09:27 +0000

    save to directory

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+const fs = require('fs');
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  socket.on('update', msg => {
-    io.emit('update', msg);
-  });
+	socket.on('update', msg => {
+		fs.writeFileSync("./files/file.txt", msg)
+		io.emit('update', msg);
+	});
 });
 
 http.listen(port, () => {
-  console.log(`running on http://localhost:${port}/`);
+	console.log(`running on http://localhost:${port}/`);
 });

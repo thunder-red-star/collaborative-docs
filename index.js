0f55a3ee8b055af43b3e331e493280ce8856fc5a
William <67127399+thunder-red-star@users.noreply.github.com>
2022-01-03 18:24:47 +0000

    serve the file

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ const port = process.env.PORT || 3000;
 const fs = require('fs');
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/files/index.html');
+});
+
+app.get('/file.txt', (req, res) => {
+	res.sendFile(__dirname + '/file.txt');
 });
 
 io.on('connection', (socket) => {
+	msg = fs.readFileSync('./files/file.txt', {encoding: "utf-8"})
+	io.emit('update', {msg:msg, status:true});
 	socket.on('update', msg => {
 		fs.writeFileSync("./files/file.txt", msg)
-		io.emit('update', msg);
+		io.emit('update', {msg:msg, status:true});
 	});
 });
 
